bb990dee365add1cfb4df59a3bd41570131aae21
jrs33 <surettej@bu.edu>
2017-04-21 13:01:59 -0400

    Added UART code to interface with bluetooth module, and clarified that PIR sensor code is working properly

--- final_proj/blink_dr.c
+++ final_proj/blink_dr.c
@@ -15,10 +15,11 @@
 
 #include <stdbool.h>
 
-volatile int state = 0;
+volatile int state = 0; // 0 = ON , 1 = OFF
+volatile uint8_t receivedValue = 0;
+volatile uint8_t checkBluetooth = 0; // 0 means we received 3, 1 means we received 4
 
 void PORT6_IRQHandler(){
-
     uint32_t status = GPIO_getEnabledInterruptStatus(GPIO_PORT_P6);
     GPIO_clearInterruptFlag(GPIO_PORT_P6, status);
 
@@ -34,6 +35,34 @@ void PORT6_IRQHandler(){
     MAP_GPIO_enableInterrupt(GPIO_PORT_P6, GPIO_PIN7);
 }
 
+const eUSCI_UART_Config uartConfig =
+{
+    EUSCI_A_UART_CLOCKSOURCE_SMCLK,          // SMCLK Clock Source
+    78,                                     // BRDIV = 78
+    2,                                       // UCxBRF = 2
+    0,                                       // UCxBRS = 0
+    EUSCI_A_UART_NO_PARITY,                  // No Parity
+    EUSCI_A_UART_LSB_FIRST,                  // MSB First
+    EUSCI_A_UART_ONE_STOP_BIT,               // One stop bit
+    EUSCI_A_UART_MODE,                       // UART mode
+    EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION  // Oversampling
+};
+
+void EUSCIA2_IRQHandler(void)
+{
+    uint32_t status = MAP_UART_getEnabledInterruptStatus(EUSCI_A2_BASE);
+
+    if(status & EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG){
+        receivedValue = MAP_UART_receiveData(EUSCI_A2_BASE);
+        if (receivedValue == 3){
+            checkBluetooth = 0;
+        }
+        else if (receivedValue == 4){
+            checkBluetooth = 1;
+        }
+    }
+}
+
 int main(void)
 {
     volatile unsigned int half_period=50000;
@@ -41,6 +70,22 @@ int main(void)
 
     /* Halting the Watchdog */
     MAP_WDT_A_holdTimer();
+
+    CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_24);
+
+    /* Selecting P3.2 and P3.3 in UART mode */
+    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P3,
+    GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+
+    /* Configuring UART Module */
+    MAP_UART_initModule(EUSCI_A2_BASE, &uartConfig);
+
+    /* Enable UART module */
+    MAP_UART_enableModule(EUSCI_A2_BASE);
+    MAP_UART_enableInterrupt(EUSCI_A2_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT);
+
+    // start with Receive Enabled
+    MAP_Interrupt_enableInterrupt(INT_EUSCIA2);
 
     /* Configuring the PIR sensor */
     MAP_Interrupt_disableSleepOnIsrExit();
@@ -58,12 +103,10 @@ int main(void)
 
     while (1)
     {
-
         /* Delay Loop */
-        for (countdown=half_period;countdown>0;--countdown){
-        }
-
-     }
+        //for (countdown=half_period;countdown>0;--countdown){
+        //}
+    }
 }
 
 
